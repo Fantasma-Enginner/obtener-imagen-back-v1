067dfe6207219fc363675efe103d5ef2d34e5d46
Fantasma Enginner <ingenieroromario@gmail.com>
2021-12-06 14:52:55 -0500

    inicial

---
+++ getimagen/src/main/java/com/getimagen/GetimagenApplication.java
@@ -0,0 +1,13 @@
+package com.getimagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetimagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetimagenApplication.class, args);
+	}
+
+}
---
+++ getimagen/src/test/java/com/getimagen/GetimagenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getimagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetimagenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
